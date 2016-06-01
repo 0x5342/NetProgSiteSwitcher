0d22a1a7913fe4028cc0a9155cf61d43ec1e64cb
0x5342 <tripe@diversedistractions.com>
2016-05-31 22:02:06 -0500

    Added a directories tab and created action listeners for the create tab

--- src/MainUI.java
+++ src/MainUI.java
@@ -1,6 +1,13 @@
+import sun.applet.Main;
+
 import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 /**
  * Created by sbrown on 5/30/2016.
@@ -12,38 +19,113 @@ public class MainUI extends JPanel {
     private JPanel mainPanel;
     private JButton createSiteButton, editSiteButton, restoreSiteButton, exitButton;
     private JTabbedPane tabbedPanel;
-    private JPanel createSitePanel;
-    private JPanel editSitePanel;
-    private JPanel restoreSitePanel;
+    private JPanel createSiteTab;
+    private JPanel editSiteTab;
+    private JPanel restoreSiteTab;
     private JLabel siteNameLabel;
     private JTextField nameOfCreateSite;
-    private JLabel tswVersionCreate;
-    private JButton chooseIMSVersionButton;
-    private JLabel imsVersionCreate;
     private JButton chooseGCCVersionButton;
+    private JTextField gccVersionCreate;
+    private JButton chooseIMSVersionButton;
+    private JTextField imsVersionCreate;
     private JButton chooseTSWVersionButton;
-    private JLabel gccVersionCreate;
+    private JTextField tswVersionCreate;
     private JCheckBox copySosCheckBoxCreate;
     private JButton createTheSiteButton;
-    private JLabel nameOfEditSite;
     private JButton siteToEditButton;
+    private JTextField nameOfEditSite;
     private JButton editGccVersionButton;
+    private JTextField gccVersionEdit;
     private JButton editImsVersionButton;
+    private JTextField imsVersionEdit;
     private JButton editTswVersionButton;
-    private JLabel gccVersionEdit;
-    private JLabel imsVersionEdit;
-    private JLabel tswVersionEdit;
+    private JTextField tswVersionEdit;
     private JCheckBox copySosCheckBoxEdit;
     private JButton saveChangesButton;
     private JButton siteToRestoreButton;
-    private JLabel siteToRestoreChosen;
+    private JTextField siteToRestoreChosen;
+    private JPanel directoriesTab;
+    private JButton changeSiteDirectoryButton;
+    private JTextField siteDirectory;
+    private JButton changeGCCRevsDirectoryButton;
+    private JTextField gccRevsDirectory;
+    private JButton changeIMSRevsDirectoryButton;
+    private JTextField imsRevsDirectory;
+    private JButton changeTSWRevsDirectoryButton;
+    private JTextField tswRevsDirectory;
+
+    JFileChooser fc;
 
     public MainUI() {
+
+        fc = new JFileChooser();
 
         exitButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
                 System.exit(0);
+            }
+        });
+        chooseGCCVersionButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                //TODO: make this start in "C:/ProgramData/Microsoft/Windows/Start Menu/Programs/Simplex/GCC/GCC REVS"
+                int returnValue = fc.showOpenDialog(MainUI.this);
+                if(returnValue==JFileChooser.APPROVE_OPTION){
+                    File file = fc.getSelectedFile();
+                    gccVersionCreate.setText(file.getName());
+                }
+            }
+        });
+        chooseIMSVersionButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                //TODO: make this start in "C:/ProgramData/Microsoft/Windows/Start Menu/Programs/Simplex/IMS/IMS REVS"
+                int returnValue = fc.showOpenDialog(MainUI.this);
+                if(returnValue==JFileChooser.APPROVE_OPTION){
+                    File file = fc.getSelectedFile();
+                    imsVersionCreate.setText(file.getName());
+                }
+            }
+        });
+        chooseTSWVersionButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                //TODO: make this start in "C:/ProgramData/Microsoft/Windows/Start Menu/Programs/Simplex/Truesite/TSW/GCCREVS"
+                int returnValue = fc.showOpenDialog(MainUI.this);
+                if(returnValue==JFileChooser.APPROVE_OPTION){
+                    File file = fc.getSelectedFile();
+                    tswVersionCreate.setText(file.getName());
+                }
+            }
+        });
+        createTheSiteButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                if(nameOfCreateSite.getText().isEmpty()){
+                    //TODO: create a dialog popup to say that a name must be entered
+                }else{
+                    //TODO: check to see if a folder with that name already exists
+                    //TODO: if it does, create a dialog popup stating so and suggestion the edit site tab
+//                    Path sitePath = Paths.get("/"+nameOfCreateSite.getText());
+//                    try {
+//                        Files.createDirectory(sitePath);
+//                    } catch (IOException e1) {
+//                        e1.printStackTrace();
+//                    }
+                    //create a directory, within the directory where the program resides, with the new site name
+                    boolean success = (new File(nameOfCreateSite.getText())).mkdir();
+                    if (!success) {
+                        // Directory creation failed
+                        //TODO: some kind of notification that the creation failed
+                    }else {//directory creation was a success
+                        if (copySosCheckBoxCreate.isSelected()) {
+                            //TODO: copy "C:/Windows/sos.ini" to the new site directory just created
+                        }
+                        //TODO: create file_links.txt with the GCC version, IMS version, & TSW version on separate lines
+                        //TODO: positive feedback that the site was created
+                    }
+                }
             }
         });
     }
